80bd12c721e7b39a2a7072cbabdb9e28841477b7
yilinliu <yilin.liu@hotmail.com>
2024-03-01 06:45:06 -0500

    async

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn-async-js
+
+Yilin Liu
--- src/quiz/animations/main.js
+++ src/quiz/animations/main.js
@@ -1,42 +1,36 @@
 const aliceTumbling = [
-    { transform: 'rotate(0) scale(1)' },
-    { transform: 'rotate(360deg) scale(0)' }
-  ];
-  
-  const aliceTiming = {
-    duration: 2000,
-    iterations: 1,
-    fill: 'forwards'
-  }
-  
-  const alice1 = document.querySelector("#alice1");
-  const alice2 = document.querySelector("#alice2");
-  const alice3 = document.querySelector("#alice3");
+  { transform: "rotate(0) scale(1)" },
+  { transform: "rotate(360deg) scale(0)" },
+];
 
-  alice1
-    .animate(aliceTumbling, aliceTiming)
-    .finished
-    .then((res) => {
-        console.log(res);
-        alice2
-            .animate(aliceTumbling, aliceTiming)
-            .finished
-            .then((res) => {
-                console.log(res);
-                alice3.animate(aliceTumbling, aliceTiming);
-            })
-    });
+const aliceTiming = {
+  duration: 2000,
+  iterations: 1,
+  fill: "forwards",
+};
 
-  // Promise chain  
-  // alice1.animate(aliceTumbling, aliceTiming).finished  
-  //   .then(() => {
-  //       return alice2
-  //               .animate(aliceTumbling, aliceTiming)
-  //               .finished;     
-  //   })
-  //   .then(() => {
-  //     return alice3
-  //             .animate(aliceTumbling, aliceTiming)
-  //             .finished;
-  //   })
-  //   .catch((err) => alert(`Error when promising ... ${err.message}`));
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+alice1.animate(aliceTumbling, aliceTiming).finished.then((res) => {
+  console.log(res);
+  alice2.animate(aliceTumbling, aliceTiming).finished.then((res) => {
+    console.log(res);
+    alice3.animate(aliceTumbling, aliceTiming);
+  });
+});
+
+// Promise chain
+// alice1.animate(aliceTumbling, aliceTiming).finished
+//   .then(() => {
+//       return alice2
+//               .animate(aliceTumbling, aliceTiming)
+//               .finished;
+//   })
+//   .then(() => {
+//     return alice3
+//             .animate(aliceTumbling, aliceTiming)
+//             .finished;
+//   })
+//   .catch((err) => alert(`Error when promising ... ${err.message}`));
